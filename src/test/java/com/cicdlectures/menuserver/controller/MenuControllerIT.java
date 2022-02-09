052c0ce6e5f1d8291522c2354c6f12084fa9d191
Adrienne9707 <lindsayzebazeadrienne@gmail.com>
2022-02-09 10:41:35 +0000

    change controllerIT  file

--- src/test/java/com/cicdlectures/menuserver/controller/MenuControllerIT.java
+++ src/test/java/com/cicdlectures/menuserver/controller/MenuControllerIT.java
@@ -1,14 +1,16 @@
 package com.cicdlectures.menuserver.controller;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.net.URL;
 import java.util.List;
-import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Arrays;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,16 +19,20 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 
-import com.cicdlectures.menuserver.repository.MenuRepository;
-import com.cicdlectures.menuserver.service.ListMenuService;
-import com.cicdlectures.menuserver.dto.MenuDto;
-import com.cicdlectures.menuserver.dto.DishDto;
-import com.cicdlectures.menuserver.model.Dish;
-import com.cicdlectures.menuserver.model.Menu;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.cicdlectures.menuserver.repository.DishRepository;
+import com.cicdlectures.menuserver.repository.MenuRepository;
+import com.cicdlectures.menuserver.model.Dish;
+import com.cicdlectures.menuserver.model.Menu;
+import com.cicdlectures.menuserver.dto.MenuDto;
+import com.cicdlectures.menuserver.dto.DishDto;
 
 
 // Lance l'application sur un port aléatoire.
@@ -38,59 +44,77 @@ public class MenuControllerIT {
     @LocalServerPort
     private int port;
 
-    private URL getMenusURL() throws Exception {
-        return new URL("http://localhost:" + port + "/menus");
-    }
-  
-    // Injecte automatiquement l'instance du menu repository
+    private URL url;
+
     @Autowired
     private MenuRepository menuRepository;
-    
-    // Injecte automatiquement l'instance du TestRestTemplate
+
+    @Autowired
+    private DishRepository dishRepository;
+
     @Autowired
     private TestRestTemplate template;
 
+    private final List<Menu> existingMenus = Arrays.asList(
+        new Menu(null, "Christmas menu", new HashSet<>(Arrays.asList(
+            new Dish(null, "Ndole", null), 
+            new Dish(null, "Eru", null)))),
+        new Menu(null, "Wedding menu", new HashSet<>(Arrays.asList(
+            new Dish(null, "Poulet DG", null), 
+            new Dish(null, "Tarte au chocolat", null)))));
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        url = new URL("http://localhost:" + port + "/menus");
+    }
+
+    @BeforeEach
+    public void initDataset() {
+        for (Menu menu : existingMenus) {
+            menuRepository.save(menu);
+        }
+    }
+
     @Test
-    @DisplayName("lists all known menus")
-    public void listsAllMenus() throws Exception {
-
-        Menu menu = new Menu(
-            null,
-            "Christmas menu", 
-            new HashSet<>(
-                Arrays.asList(
-                    new Dish(null, "Ndole", null),
-                    new Dish(null, "Eru", null)
-                )
-            )
-        );
-
-        menuRepository.save(menu);
-  
-        // Effectue une requête GET /menus
-        ResponseEntity<MenuDto[]> response = this.template.getForEntity(getMenusURL().toString(), MenuDto[].class);
-  
-        //Parse le payload de la réponse sous forme d'array de MenuDto
-        List<MenuDto> gotMenus = Arrays.asList(response.getBody());
-          
-        // Verifie que le status de la réponse est 200
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-          
-        // On défini wantMenus, les résultats attendus
-        List<MenuDto> wantMenus = Arrays.asList(
-            new MenuDto(
-                Long.valueOf(1),
-                "Christmas menu",
-                new HashSet<>(
+    public void listsExistingMenus() throws Exception {
+        MenuDto[] wantMenus = {
+            new MenuDto(Long.valueOf(1), "Christmas menu",
+                new HashSet<DishDto>(
                     Arrays.asList(
-                        new DishDto(Long.valueOf(1), "Ndole"),
-                        new DishDto(Long.valueOf(2), "Eru")
-                    )
-                )
-            )
-        );
-      
-        // On compare la valeur obtenue avec la valeur attendue.
-        assertEquals(wantMenus, gotMenus);
+                        new DishDto(Long.valueOf(1), "Ndole"), 
+                        new DishDto(Long.valueOf(2), "Eru")))),
+            new MenuDto(Long.valueOf(2), "Wedding menu", new HashSet<DishDto>(
+                    Arrays.asList(
+                    new DishDto(Long.valueOf(3), "Poulet DG"), 
+                    new DishDto(Long.valueOf(4), "Tarte au chocolat")))) };
+                    
+                    ResponseEntity<MenuDto[]> response = this.template.getForEntity(url.toString(), MenuDto[].class);
+                    MenuDto[] gotMenus = response.getBody();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertArrayEquals(wantMenus, gotMenus);
+    }
+
+    @Test
+    public void createsNewMenu() throws Exception {
+        MenuDto wantMenu = new MenuDto(Long.valueOf(3), "Dinner's family menu", new HashSet<DishDto>(
+            Arrays.asList(
+                new DishDto(Long.valueOf(1), "Ndole"), 
+                new DishDto(Long.valueOf(2), "Eru"))));
+
+        MenuDto newMenu = new MenuDto(null, "Dinner's family menu",
+            new HashSet<DishDto>(Arrays.asList(
+                new DishDto(null, "Ndole"), 
+                new DishDto(null, "Eru"))));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<MenuDto> request = new HttpEntity<>(newMenu, headers);
+
+        ResponseEntity<MenuDto> response = this.template.postForEntity(url.toString(), request, MenuDto.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(wantMenu, response.getBody());
     }
 }

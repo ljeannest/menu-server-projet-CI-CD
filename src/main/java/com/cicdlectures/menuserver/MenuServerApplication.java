aea16a2f328a52b6beb37d9fd95ad4c2aa60e5f8
Adrienne9707 <lindsayzebazeadrienne@gmail.com>
2022-02-11 11:12:51 +0000

    add console method

--- src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
+++ src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
@@ -3,11 +3,12 @@ package com.cicdlectures.menuserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class MenuServerApplication {
 
+
+@SpringBootApplication
+
+public class MenuServerApplication {
   public static void main(String[] args) {
     SpringApplication.run(MenuServerApplication.class, args);
   }
-
 }

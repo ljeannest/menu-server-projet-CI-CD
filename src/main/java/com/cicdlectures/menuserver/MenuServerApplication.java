a224bf362c4d831da409f0e2cc5df3129d64c6d6
Adrienne9707 <lindsayzebazeadrienne@gmail.com>
2022-02-13 11:17:33 +0000

    add delete file Roa'a

--- src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
+++ src/main/java/com/cicdlectures/menuserver/MenuServerApplication.java
@@ -3,26 +3,10 @@ package com.cicdlectures.menuserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-
-
-
 @SpringBootApplication
 
-@Command(
-  name = "hello",
-  description = "Says hello"
-)
 public class MenuServerApplication {
   public static void main(String[] args) {
     SpringApplication.run(MenuServerApplication.class, args);
-    //CommandLine.run(new MenuServerApplication(), args);
   }
-
-  /*@Override
-  public void run() {
-    System.out.println("Hello World!");
-    
-  }*/
 }
